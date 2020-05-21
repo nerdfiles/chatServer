7e5b6f3b951ec6e96d050728a26e6b5e4e650e94
nerdfiles <nerdfiles@gmail.com>
2020-05-21 15:44:32 -0500

    Auth fixed

--- server/chat-server.js
+++ server/chat-server.js
@@ -8,20 +8,25 @@ const ws = new WebSocket.Server({port: PORT});
 console.log(`started ws on ${PORT}`)
 
 ws.on('connection', (ws) => {
+
   function login(email, pass) {
-    models.User.login(email, pass, (err, result) => {
+    models.User.login({
+      email: email,
+      password: pass
+    }, (err, result) => {
       if (err) {
-        ws.sendJSON.stringify({
+        console.log('logging in failed')
+        ws.send(JSON.stringify({
           type: 'ERROR',
           error: err
-        })
+        }));
       } else {
         models.User.findOne({ where: {id : result.userId}, include: 'Profile'}, (errFindOne, user) => {
           if (errFindOne) {
-            ws.sendJSON.stringify({
+            ws.send(JSON.stringify({
               type: 'ERROR',
               error: errFindOne
-            })
+            }));
           } else {
             ws.send(JSON.stringify({
               type: 'LOGGEDIN',
@@ -37,32 +42,58 @@ ws.on('connection', (ws) => {
   }
 
   ws.on('message', message => {
-    console.log('got message', JSON.parse(message));
-    let parsed = JSON.parsed(message)
+
+    let parsed = JSON.parse(message)
+
     if (parsed) {
-      switch(parsed.type) {
+
+      console.log('parsed successfully', parsed)
+
+      switch (parsed.type) {
+
         case 'SIGNUP':
+          console.log('SIGNUP');
+          console.log(parsed.data)
           models.User.create(parsed.data, (err, user) => {
+
             if (err) {
+              console.log('User not created', err)
+
               ws.send(JSON.stringify({
                 type: 'ERROR',
                 error: err
-              }))
+              }));
+
             } else {
+              console.log('User created', user);
+
               models.Profile.create({
                 userId: user.id,
                 name: parsed.data.name,
                 email: parsed.data.email
               }, (profileErr, profile) => {
 
-              })
+                if (!profileErr) {
+                  console.log('Profile created', profile)
+                  login(parsed.data.email, parsed.data.password)
+                } else {
+                  console.log('Profile not created', profileErr)
+                }
+
+              });
             }
-          })
+          });
+          break;
+
         case 'LOGIN':
+          console.log('loggin in', parsed.data)
           login(parsed.data.email, parsed.data.password);
+          break;
+
         default:
-          console.log('Nothing to see here')
+          console.log('Nothing to see here');
       }
     }
   });
 });
+
